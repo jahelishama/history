57fd459d82a610b3e34da83e65f8d714b4d4f750
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-05 19:19:50 -0700

    History quiz version 1.o

---
+++ src/Quiz.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class Quiz {
+public static void main(String[] args) {
+	String ancient = JOptionPane.showInputDialog("Who helped create hellinistic kingdoms");
+	if (ancient.equalsIgnoreCase("alexander the great")) {
+		JOptionPane.showMessageDialog(null,"Noice");
+		
+		
+	
+	}
+	else {
+		
+		
+			JOptionPane.showMessageDialog(null,"sorry dude if you were a girl i am sorry eh eh eh ok back to da vid you got dat wrong yoyoyoy" );
+		
+	    }
+	}
+}
